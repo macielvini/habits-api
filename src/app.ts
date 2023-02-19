ba41c9142625ebc15ab205f7a6bd59212887899c
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-02-18 21:29:49 -0300

    chore: fastify cors setup

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = Fastify();
+
+app.register(cors);
 
 app.get("/health", () => {
   return "Health Check";
